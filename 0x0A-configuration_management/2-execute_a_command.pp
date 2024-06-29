3264d247e00dafa91688dfe297ea1ff5b37f39d5
balogun abdulmalik <balogunabdulmalik04@gmail.com>
2024-06-29 05:36:16 +0100

    learning cm

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-# Create a Puppet manifest to kill a process named "killmenow" using pkil
+# Create a Puppet manifest to kill a process named "killmenow"
 
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill -f /alx-system_engineering-devops/0x0A-configuration_management/killmenow',
-  onlyif      => '/usr/bin/pgrep -f /alx-system_engineering-devops/0x0A-configuration_management/killmenow', # Check if the process is running before attempting to kill it
-  refreshonly => true,
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
