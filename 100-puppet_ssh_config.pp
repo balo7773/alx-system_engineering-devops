97cb1ac78c1ca6e6be3bd5f4a8bd9b21821093a1
balo7773 <balogunabdulmalik04@gmail.com>
2024-06-30 16:40:55 -0700

    learning ssh

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# manifest to setup ssh config file
+
+file { '/etc/ssh/ssh_config':
+   ensure  => 'present',
+   content => "
+    Host *
+        SendEnv LANG LC_*
+        HashKnownHosts yes
+        GSSAPIAuthentication yes
+        GSSAPIDelegateCredentials no
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+    "
+}
